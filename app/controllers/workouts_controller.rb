b758d87f999161a0d0b46d67237958d676cddfbc
Joe Bigio <jvbigio@gmail.com>
2018-12-10 20:41:12 +0000

    Data saved on submit

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -7,4 +7,15 @@ class WorkoutsController < ApplicationController
   def new
     @workout = Workout.new
   end
+
+  def create
+    Workout.create(workout_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:exercise, :author)
+  end
 end
